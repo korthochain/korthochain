600fc835ce907699e904a5dcdbb3717c778f0004
yiming company <1645398@qq.com>
2021-08-30 16:42:50 +0800

    add partial method,modify set,new comments

--- pkg/blockchain/interface.go
+++ pkg/blockchain/interface.go
@@ -9,16 +9,26 @@ import (
 
 //Blockchains interface specification of blockchain
 type Blockchains interface {
+	// NewBlock create a new block for the blockchain
 	NewBlock([]*transaction.SignedTransaction, address.Address) (*block.Block, error)
+	// AddBlock add blocks to blockchain
 	AddBlock(*block.Block) error
+	// DeleteBlock delete some blocks from the blockchain
 	DeleteBlock(uint64) error
-
+	// GetBalance retrieves the balance from the given address or 0 if object not found
 	GetBalance([]byte) (uint64, error)
-	GetFreezeBalance(address []byte) (uint64, error)
+	// GetFreezeBalance get the freeze balance of the address
+	GetFreezeBalance([]byte) (uint64, error)
+	// GetAvailableBalance get available balance of address
+	GetAvailableBalance([]byte) (uint64, error)
+	// GetNonce get the nonce of the address
 	GetNonce([]byte) (uint64, error)
+	// GetHash get the hash corresponding to the block height
 	GetHash(uint64) ([]byte, error)
+	// GetMaxBlockHeight get maximum block height
 	GetMaxBlockHeight() (uint64, error)
-	GetHeight() (uint64, error)
+	// GetBlockByHeight get the block corresponding to the block height
 	GetBlockByHeight(uint64) (*block.Block, error)
-	GetTransactionByHash([]byte) (*transaction.Transaction, error)
+	// GetTransactionByHash get the transaction corresponding to the transaction hash
+	GetTransactionByHash([]byte) (*transaction.SignedTransaction, error)
 }
